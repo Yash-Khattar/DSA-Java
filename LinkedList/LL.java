bee3fee741014093ce31722e0b1c5fdbe1852338
Yash-Khattar <yashkhattar73@gmail.com>
2024-04-08 19:46:50 +0530

    LL

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -5,35 +5,36 @@ public class LL {
     private Node tail;
 
     private int size;
-    public LL(){
-        this.size =0;
+
+    public LL() {
+        this.size = 0;
     }
-// insert node
-    public void insertFirst(int val){
+
+    // insert node
+    public void insertFirst(int val) {
         Node node = new Node(val);
         node.next = head;
         head = node;
-        if(tail == null){
+        if (tail == null) {
             tail = head;
         }
 
         size++;
     }
 
-// display LL
-    public void display(){
+    // display LL
+    public void display() {
         Node temp = head;
-        while(temp!= null){
+        while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
     }
 
-
-// insert last
-    public void insertLast(int val){
+    // insert last
+    public void insertLast(int val) {
         Node node = new Node(val);
-        if(tail == null){
+        if (tail == null) {
             insertFirst(val);
             return;
         }
@@ -43,32 +44,107 @@ public class LL {
 
     }
 
-// insert at index
-    public void insertIndex(int index, int val){
+    // insert at index
+    public void insertIndex(int index, int val) {
 
-    Node temp = head;
-     while(index!=1){
-        temp = temp.next;
-        index--;
-     }
-     
-    Node node = new Node(val, temp.next);
-    temp.next = node;
+        Node temp = head;
+        while (index != 1) {
+            temp = temp.next;
+            index--;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+
+        size++;
+    }
+
+    // insert recursion
+    public void insertRec(int val, int index) {
+        recursion(val, index, head);
+    }
+
+    private Node recursion(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+        node.next = recursion(val, index--, node.next);
+        return node;
+    }
+
+    // merge
+    public static LL merge(LL first, LL second) {
+        Node f = first.head;
+        Node s = second.head;
+        LL mergedLL = new LL();
+        while(f != null && s != null){
+            if(f.value < s.value){
+                mergedLL.insertFirst(f.value);
+                f = f.next;
+            }else{
+                mergedLL.insertFirst(s.value);
+                s = s.next;
+            }
+        }
+        while( s!= null){
+            mergedLL.insertFirst(s.value);
+            s = s.next;
+        }
+        while( f!= null){
+            mergedLL.insertFirst(f.value);
+            f = f.next;
+        }
+
+        return mergedLL;
+
+    }
+
+    // get mid 
+    public static Node mid(Node head){
+    Node s = head;
+Node f = head;
+while(f.next != null){
+    f = f.next.next;
+    s = s.next;
     
-     size++; 
+}
+return s;
     }
-    //inner class
-    private class Node {
-    private int value;
-    private Node next;
 
-    public Node(int value){
-        this.value = value;
-    }
-    public Node(int value, Node next){
-        this.next = next;
-        this.value = value;
-    }
+    // reverse a LL
+    public static void reverse(){
         
+    }
+
+
+    // merge sort 
+//     public static Node mergeSort(Node head){
+//         if(head == null || head.next == null){
+//             return head;
+//         }
+//         Node mid = mid(head);
+//         Node left = mergeSort(head);
+//         Node right = mergeSort(mid);
+        
+// return merge(left, right);
+
+    // }
+
+    // inner class
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.next = next;
+            this.value = value;
+        }
+
     }
 }
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,6 +1,9 @@
 package LinkedList;
 
 public class Main {
+
+
+
     public static void main(String[] args) {
         DoublyLL list = new DoublyLL();
         list.insertFirst(1);
